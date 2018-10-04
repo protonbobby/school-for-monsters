5498404c97b8e26b3add0a86e454917558fd05ce
Bobby Hirtle <protonbobby@gmail.com>
2018-10-04 19:06:38 -0400

    removed old selector

--- src/store.js
+++ src/store.js
@@ -12,18 +12,3 @@ const reducer = combineReducers({
 });
 
 export default createStore(reducer, applyMiddleware(thunk, logger));
-
-//______________________selectors_________________________________________
-export const fetch = (id) => {
-  // type = type.toString();
-  // type === 'schools' ? type : type = 'students';
-  return axios.get(`/school/${id}`)
-    .then(res => res.data);
-}
-
-export const matchObject = (obj, matchObj) => {
-  return Object.keys(matchObj).reduce((isMatching, key) => {
-    return isMatching && obj[key] === matchObj[key]
-  }, true);
-};
-
